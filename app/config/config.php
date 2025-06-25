255a224f504c6d3b05f7f9c457bd5cc6bcf0c4af
Mohamed-M-M-Ramadan <167596723+Mohamed-M-M-Ramadan@users.noreply.github.com>
2025-06-25 13:20:50 +0800

    Final changes to launch the website

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'recipe_planner');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql103.infinityfree.com');
+define('DB_NAME', 'if0_39316706_recipe_planner');
+define('DB_USER', 'if0_39316706');
+define('DB_PASS', 'jBMQYO1cygA');
 
 // Application paths
 define('BASE_URL', 'http://localhost/recipe_meal_planner/');
@@ -15,10 +15,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Database connection
-$host = 'localhost';
-$db   = 'recipe_planner';
-$user = 'root';
-$pass = '';
+$host = 'sql103.infinityfree.com';
+$db   = 'if0_39316706_recipe_planner';
+$user = 'if0_39316706';
+$pass = 'jBMQYO1cyg';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
